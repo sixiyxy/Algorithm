47d2c4bbb044717419fa7bc2cd5d566512069863
sixiyxy <771702500@qq.com>
2021-09-24 11:20:31 +0800

    1

---
+++ 剑指offer/test.js
@@ -0,0 +1,16 @@
+
+
+
+res = []
+temp = []
+path = [1,2,3]
+
+res.push(path);
+temp.push([...path])
+
+res.push(path);
+temp.push([...path])
+res.push(path);
+temp.push([...path])
+res.push(path);
+temp.push([...path])
